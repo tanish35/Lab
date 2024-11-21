152e9b5d330d81a27fe0895617840f1c81188a86
tanish35 <tanishmajumdar2912@gmail.com>
2024-11-21 21:04:51 +0530

    Add new questions

--- DBMSLab/pl_sql/q4.sql
+++ DBMSLab/pl_sql/q4.sql
@@ -1,15 +1,21 @@
 DECLARE
     a NUMBER ;
-    r NUMBER;
+    b NUMBER;
+    c char(1);
     ans NUMBER;
 BEGIN
-  a:=563847384;
-  ans:=0;
-  WHILE a!=0 LOOP
-    r:=MOD(a,10);
-    ans:=ans*10+r;
-    a:=TRUNC(a/10);
-  END LOOP;
+  a:=50;
+  b:=10;
+  c:='*';
+  IF c='+' THEN
+    ans:=a+b;
+  ELSIF c='-' THEN
+    ans:=a-b;
+  ELSIF c='/' THEN
+    ans:=a/b;
+  ELSE
+    ans:=a*b;
+  END IF;
   dbms_output.put_line('The ans is '||ans);
 END;
 
